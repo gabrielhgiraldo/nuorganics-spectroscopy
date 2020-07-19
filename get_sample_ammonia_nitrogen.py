bc989dab9f212d7ab9a17ec041b1c1b8401930f4
Gabriel Giraldo-Wingler <gabriel@GG.fios-router.home>
2020-07-19 14:00:01 -0400

    functioning script

--- get_sample_ammonia_nitrogen.py
+++ get_sample_ammonia_nitrogen.py
@@ -1,8 +1,11 @@
 import argparse
 from pathlib import Path
+import os
+
+import pandas as pd
 
 from spectroscopy.model import load_model
-from spectroscopy.utils import parse_trms
+from spectroscopy.utils import get_wavelength_columns, parse_trms
 
 # Construct the argument parser
 ap = argparse.ArgumentParser()
@@ -12,10 +15,26 @@ ap.add_argument("-sp", "--sample-path", required=True,
    help="path to sample files")
 args = vars(ap.parse_args())
 
+print('loading model')
 model = load_model()
-sample_path = Path(args['sample-path'])
-
-df_samples = parse_trms(sample_path)
-ammonia_df = model.predict(df_samples)
-ammonia_df.to_csv(f'ammonia_N.csv')
+sample_path = Path(args['sample_path'])
+print('extracting samples')
+df_samples = pd.DataFrame()
+try:
+    df_samples = parse_trms(sample_path)
+    print('samples extracted')
+except FileNotFoundError:
+    print(f'no samples found at the provided path {sample_path}')
+    os.system.exit()
+print('calculating samples Ammonia-N')
+wavelength_columns = get_wavelength_columns(df_samples)
+X_samples = df_samples[wavelength_columns]
+ammonia_N = model.predict(X_samples)
+results_df = pd.DataFrame({
+    'filename':df_samples['filename'],
+    'Ammonia-N':ammonia_N
+})
+results_path = Path('ammonia_N.csv')
+print(f'saving samples Ammonia-N to {results_path}')
+results_df.to_csv(results_path, index=False)
 
