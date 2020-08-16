52357b702fa6ff548836589153539b24569e1652
Gabriel Giraldo-Wingler <gabriel@GG.fios-router.home>
2020-08-16 17:59:48 -0400

    bugfix:removed unused torch import

--- spectroscopy/model.py
+++ spectroscopy/model.py
@@ -15,8 +15,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 # from sklearn.decomposition import PCA
 # from skorch.regressor import NeuralNetRegressor
-from torch import nn
-import torch.nn.functional as F
+# from torch import nn
+# import torch.nn.functional as F
 
 
 from spectroscopy.utils import (
@@ -31,26 +31,26 @@ MODEL_METRICS_FILENAME = 'scores.json'
 
 logger = logging.getLogger(__name__)
 
-class RegressorModule(nn.Module):
-    def __init__(
-            self,
-            num_units=10,
-            nonlin=F.relu,
-    ):
-        super(RegressorModule, self).__init__()
-        self.num_units = num_units
-        self.nonlin = nonlin
+# class RegressorModule(nn.Module):
+#     def __init__(
+#             self,
+#             num_units=10,
+#             nonlin=F.relu,
+#     ):
+#         super(RegressorModule, self).__init__()
+#         self.num_units = num_units
+#         self.nonlin = nonlin
 
-        self.dense0 = nn.Linear(20, num_units)
-        self.nonlin = nonlin
-        self.dense1 = nn.Linear(num_units, 10)
-        self.output = nn.Linear(10, 1)
+#         self.dense0 = nn.Linear(20, num_units)
+#         self.nonlin = nonlin
+#         self.dense1 = nn.Linear(num_units, 10)
+#         self.output = nn.Linear(10, 1)
 
-    def forward(self, X, **kwargs):
-        X = self.nonlin(self.dense0(X))
-        X = F.relu(self.dense1(X))
-        X = self.output(X)
-        return X
+#     def forward(self, X, **kwargs):
+#         X = self.nonlin(self.dense0(X))
+#         X = F.relu(self.dense1(X))
+#         X = self.output(X)
+#         return X
 
 def mean_absolute_percentage_error(y_true, y_pred): 
     # y_true, y_pred = check_arrays(y_true, y_pred)
