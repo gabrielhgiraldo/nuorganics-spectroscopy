e8f48937e6e1bfd6bd678cad64499e60bb648452
Gabriel Giraldo-Wingler <gabriel@GG.fios-router.home>
2020-08-16 15:51:31 -0400

    switched categorical variable transformation to sklearn pipeline using OneHotEncoder and ColumnTransformer

--- spectroscopy/model.py
+++ spectroscopy/model.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from pathlib import Path
 
 from lightgbm import LGBMRegressor
@@ -6,12 +7,13 @@ import numpy as np
 import pandas as pd
 import pickle
 from pprint import pprint
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
 from skorch.regressor import NeuralNetRegressor
 from torch import nn
@@ -19,13 +21,16 @@ import torch.nn.functional as F
 
 
 from spectroscopy.utils import (
-    load_extracted_training_data,
+    TRAINING_DATA_FILENAME,
+    load_extracted_data,
     get_wavelength_columns,
     plot_fit,
 )
 MODEL_DIR = Path('bin/model/')
 MODEL_FILENAME = 'model.pkl'
 MODEL_METRICS_FILENAME = 'scores.json'
+
+logger = logging.getLogger(__name__)
 
 class RegressorModule(nn.Module):
     def __init__(
@@ -70,7 +75,7 @@ def score_model(model, X_train, y_train, X_test, y_test):
         'test_rmse':np.sqrt(mean_squared_error(y_test, y_test_pred))
     }
 
-def _define_model():
+def define_model():
 #     return NeuralNetRegressor(
 #         RegressorModule,
 #         max_epochs=20,
@@ -79,15 +84,33 @@ def _define_model():
 #     )
     # return RandomForestRegressor(random_state=10, max_depth=20, n_estimators=100)
     # return LGBMRegressor()
-    return RandomForestRegressor(random_state=10)    
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('categorical', OneHotEncoder(), ['process_method'])
+        ],
+        remainder='passthrough'
+    )
+    return Pipeline(steps=[
+        ('preprocess', preprocessor),
+        ('model', RandomForestRegressor(random_state=10))
+    ])
 
-# TODO: get ammonia features vs get moisture features
 def get_features(df):
     feature_columns = get_wavelength_columns(df)
     feature_columns.append('integration_time')
-    feature_columns.extend(df['process_method'].unique())
- 
+    feature_columns.append('process_method')
     return feature_columns
+
+# TODO: make this an sklearn transformer or series of transformers
+# TODO: enable different features for different models
+def transform_data(df):
+    df['process_method'].fillna('none', inplace=True)
+    df['process_method'] = df['process_method'].astype(str)
+    # df = df[df['process_method'].isin(['ground','wet'])]
+    # df = pd.concat([df, pd.get_dummies(df['process_method'])], axis=1)
+    feature_columns = get_features(df)
+    X = df[feature_columns]
+    return X
 
 
 # TODO: generalize this for multiple targets
@@ -95,29 +118,24 @@ def train_models(targets, model_dir=None, training_data_path=None):
     if model_dir is None:
         model_dir = MODEL_DIR
     model_dir = Path(model_dir)
-    df = load_extracted_training_data(training_data_path)
-    # TODO: have preprocessing and feature extraction occur in model pipeline
-    df['process_method'].fillna('none', inplace=True)
-    df['process_method'] = df['process_method'].astype(str)
-    # df = df[df['process_method'].isin(['ground','wet'])]
-    df = pd.concat([df, pd.get_dummies(df['process_method'])], axis=1)
-    # TODO: enable different features for different models
-    feature_columns = get_features(df)
-    X = df[feature_columns]
+    df = load_extracted_data(training_data_path, TRAINING_DATA_FILENAME)
+    # TODO: make this an sklearn transformer
+    X = transform_data(df)
+    # TODO: have feature extraction occur in model pipeline
     # TODO: switch printing with logging
     # TODO: add ability to include different experiments in one training run
     for target in targets:
-        print(f'Fitting {target} model')
+        logger.info(f'Fitting {target} model')
         target_model_dir = model_dir / target
         y = df[target]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
-        print(f'total samples:{len(df)} training on:{len(X_train)} testing on {len(X_test)}')
+        logger.info(f'total samples:{len(df)} training on:{len(X_train)} testing on {len(X_test)}')
         # TODO: allow for different architectures for each model
-        model = _define_model()
+        model = define_model()
         model.fit(X_train, y_train)
         scores = score_model(model, X_train, y_train, X_test, y_test)
-        pprint(scores)
-        print('saving fit graph')
+        logger.info(pprint(scores))
+        logger.info('saving fit graph')
         plot_fit(y_test, model.predict(X_test))
         target_model_dir.mkdir(parents=True, exist_ok=True)
         # TODO: use database or experiment handling framework for metrics storage
